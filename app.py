ee0228a8d0095169fe17e70aa344ef41147e8dd4
otootouraaka-ship-it <otootouraaka@gmail.com>
2026-05-13 11:36:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import base64
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -22,6 +24,40 @@ st.set_page_config(
     page_title="げんしけんにじさんじ共通テスト2026 in 清陵祭",
     layout="wide"
 )
+
+def set_bg_image(image_file):
+
+    with open(image_file, "rb") as f:
+        data = f.read()
+
+    encoded = base64.b64encode(data).decode()
+
+    page_bg_img = f"""
+    <style>
+    
+    .stApp {{
+        background-image:
+            linear-gradient(
+                rgba(0,0,0,0.5),
+                rgba(0,0,0,0.5)
+            ),
+            url("data:image/jpg;base64,{encoded}");
+    
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    
+    </style>
+    """
+
+    st.markdown(
+        page_bg_img,
+        unsafe_allow_html=True
+    )
+
+set_bg_image("image.png")
 
 st.title("げんしけんにじさんじ共通テスト2026 in 清陵祭")
 
